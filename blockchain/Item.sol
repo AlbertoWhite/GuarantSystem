9cedebec1a26f49fb840e8e32766649c2a2ff82e
ymeister <47071325+ymeister@users.noreply.github.com>
2019-01-28 18:17:41 +0700

    Item changes (mostly syntax).

--- blockchain/Item.sol
+++ blockchain/Item.sol
@@ -1,30 +1,27 @@
 pragma solidity ^0.5.1;
- 
+
 contract Item {
-       address id = address(this);
-       string physicalAddress;
-       string info;
-       address manfactureID;
-       address sellerID;
-       address ownerID;
-       address probablyNewOwnerID;
-       struct createdDate {
-           uint16 year;
-           uint8 month;
-           uint8 day;
-        }
-        uint 16 guareanteePeriod;
-        string warrantyTerms;
-        struct activatedDate {
-           uint16 year;
-           uint8 month;
-           uint8 day;
-           uint8 hour;
-           uint8 minute;
-        }
-        string status;
-        // тип под вопросом   history []; 
-      
-       constructor() internal {
-       ownerID = msg.sender;
-     }
+    address id = address(this);
+    string serial;
+    string info;
+    address manfacturerID;
+    address vendorID;
+    address ownerID;
+    Time created;
+    Time warrantyPeriod;
+    string warrantyTerms;
+    Time activated;
+
+    // Status status;
+    // Action[] history;
+
+    constructor() internal {
+        ownerID = msg.sender;
+    }
+
+    struct Time {
+        uint16 year;
+        uint8 month;
+        uint8 day;
+    }
+}
