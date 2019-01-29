5d8ddbe47178d8468631936a407c65e0ba582d62
ymeister <47071325+ymeister@users.noreply.github.com>
2019-01-29 16:30:04 +0700

    Added modifiers and mappings

--- blockchain/contract.sol
+++ blockchain/contract.sol
@@ -34,11 +34,22 @@ contract Manufacturer {
     string name;
     string physicalAddress;
     string registrationNumber;
+
     address[] vendors;
     address[] serviceCenters;
     address[] pendingVendors;
     address[] pendingServiceCenters;
     address[] pendingItems;
+
+
+
+    mapping (address => bool) isVendor;
+    mapping (address => bool) isServiceCenter;
+    mapping (address => bool) isPendingVendor;
+    mapping (address => bool) isPendingServiceCenter;
+    mapping (address => bool) isPendingItem;
+
+
 
     constructor (string memory _name, string memory _physicalAddress, string memory _registrationNumber) public {
         ownerID = msg.sender;
@@ -47,81 +58,123 @@ contract Manufacturer {
         registrationNumber = _registrationNumber;
     }
 
+
+
+    modifier onlyOwner {
+        require(msg.sender == ownerID, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyOwnerOrVendor {
+        require(((msg.sender == ownerID) || (isVendor[msg.sender] == true)), "Only owner or partner vendor can call this function");
+        _;
+    }
+
+    modifier onlyOwnerOrServiceCenter {
+        require(((msg.sender == ownerID) || (isServiceCenter[msg.sender] == true)), "Only owner or partner service center can call this function");
+        _;
+    }
+
+
+
+    function _removeFromAddressArray (address[] array, address value) internal {
+        for (uint i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+              array[i] = array[array.length - 1];
+              delete array[array.length - 1];
+              array.length = array.length - 1;
+            }
+        }
+    }
+
+    function _addPendingItem (address iID) internal {
+        require(isPendingItem[iID] == false, "Item is already on the pending list");
+
+        pendingItems.push(iID);
+        isPendingItem[iID] = true;
+    }
+
+    function _addVendor (address vID) internal {
+        require(isVendor[vID] == false, "Vendor is already partnered with");
+
+        vendors.push(vID);
+        isVendor[vID] = true;
+    }
+
+    function _addServiceCenter (address scID) internal {
+        require(isServiceCenter[scID] == false, "Service Center is arleady partnered with");
+
+        serviceCenters.push(scID);
+        isServiceCenter[scID] = true;
+    }
+
+    function receiveVendor () public {
+        address vID = msg.sender;
+        require(isPendingVendor[vID] == false, "Partnership request has already been sent");
+
+        pendingVendors.push(vID);
+        isPendingVendor[vID] = true;
+    }
+
+    function receiveServiceCenter () public {
+        address scID = msg.sender;
+        require(isPendingServiceCenter[scID] == false, "Partnership request has already been sent");
+
+        pendingServiceCenters.push(scID);
+        isPendingServiceCenter[scID] = true;
+    }
+
+
+
     function createItem (string memory _serial, string memory _info, uint _warrantyPeriod, string memory _warrantyTerms) onlyOwner public {
         Item newItem = new Item(_serial, _info, _warrantyPeriod, _warrantyTerms);
-        pendingItems.push(newItem.id());
+        _addPendingItem(address(newItem));
     }
 
-    function addVendor (address v) onlyOwner public {
-        for (uint i = 0; i < pendingVendors.length; i++) {
-            if (v == pendingVendors[i]) {
-                vendors.push(v);
-                delete pendingVendors[i];
-                return;
-            }
-        }
-        Vendor vInstance = Vendor(v);
-        vInstance.receiveManufacturer(id);
-    }
 
-    function receiveVendor (address v) public {
-        for (uint i = 0; i < pendingVendors.length; i++) {
-            if (v == pendingVendors[i]) {
-                return;
-            }
-        }
-        pendingVendors.push(v);
-    }
 
-    function removeVendor (address v) onlyOwnerOrVendor public {
-        bool exists = false;
-        for (uint i = 0; i < vendors.length; i++) {
-            if (v == vendors[i]) {
-                delete vendors[i];
-                exists = true;
-                break;
-            }
-        }
-        if (exists == true) {
-            Vendor vInstance = Vendor(v);
-            vInstance.removeManufacturer(id);
+    function addVendor (address vID) onlyOwner public {
+        if (isPendingVendor[vID] == true) {
+            _addVendor(vID);
+            _removeFromAddressArray(pendingVendors, vID);
+            isPendingVendor[vID] = false;
+        } else {
+            Vendor vInstance = Vendor(vID);
+            vInstance.receiveManufacturer();
         }
     }
 
-    function addServiceCenter (address sc) onlyOwner public {
-        for (uint i = 0; i < pendingServiceCenters.length; i++) {
-            if (sc == pendingServiceCenters[i]) {
-                serviceCenters.push(sc);
-                delete pendingServiceCenters[i];
-                return;
-            }
-        }
-        ServiceCenter scInstance = ServiceCenter(sc);
-        scInstance.receiveManufacturer(id);
+    function removeVendor (address vID) onlyOwnerOrVendor public {
+        require(isVendor[vID] == true, "No such vendor");
+
+        _removeFromAddressArray(vendors, vID);
+        isVendor[vID] = false;
+
+        Vendor vInstance = Vendor(vID);
+        vInstance.removeManufacturer(id);
     }
 
-    function receiveServiceCenter (address sc) public {
-        for (uint i = 0; i < pendingServiceCenters.length; i++) {
-            if (sc == pendingServiceCenters[i]) {
-                return;
-            }
+
+
+    function addServiceCenter (address scID) onlyOwner public {
+        if (isPendingServiceCenter[scID] == true) {
+            _addServiceCenter(scID);
+            _removeFromAddressArray(pendingServiceCenters, scID);
+            isPendingServiceCenter[scID] = false;
+        } else {
+            ServiceCenter scInstance = ServiceCenter(scID);
+            scInstance.receiveManufacturer();
         }
-        pendingServiceCenters.push(sc);
     }
 
-    function removeServiceCenter (address sc) onlyOwnerOrServiceCenter public {
-        bool exists = false;
-        for (uint i = 0; i < serviceCenters.length; i++) {
-            if (sc == serviceCenters[i]) {
-                delete serviceCenters[i];
-                exists = true;
-                break;
-            }
-        }
-        if (exists == true) {
-            ServiceCenter scInstance = ServiceCenter(sc);
-            scInstance.removeManufacturer(id);
-        }
+    function removeServiceCenter (address scID) onlyOwnerOrServiceCenter public {
+        require(isServiceCenter[scID] == true, "No such service center");
+
+        _removeFromAddressArray(serviceCenters, scID);
+        isServiceCenter[scID] = false;
+
+        ServiceCenter scInstance = ServiceCenter(scID);
+        scInstance.removeManufacturer(id);
     }
 }
 
@@ -133,8 +186,18 @@ contract Vendor {
     string name;
     string physicalAddress;
     string registrationNumber;
+
     address[] manufacturers;
     address[] pendingManufacturers;
+    address[] pendingItems;
+
+
+
+    mapping (address => bool) isManufacturer;
+    mapping (address => bool) isPendingManufacturer;
+    mapping (address => bool) isPendingItem;
+
+
 
     constructor (string memory _name, string memory _physicalAddress, string memory _registrationNumber) public {
         ownerID = msg.sender;
@@ -143,40 +206,73 @@ contract Vendor {
         registrationNumber = _registrationNumber;
     }
 
-    function addManufacturer (address m) onlyOwner public {
-        for (uint i = 0; i < pendingManufacturers.length; i++) {
-            if (m == pendingManufacturers[i]) {
-                manufacturers.push(m);
-                delete pendingManufacturers[i];
-                return;
-            }
-        }
-        Manufacturer mInstance = Manufacturer(m);
-        mInstance.receiveVendor(id);
+
+
+    modifier onlyOwner {
+        require(msg.sender == ownerID, "Only owner can call this function");
+        _;
     }
 
-    function receiveManufacturer (address m) public {
-        for (uint i = 0; i < pendingManufacturers.length; i++) {
-            if (m == pendingManufacturers[i]) {
-                return;
-            }
-        }
-        pendingManufacturers.push(m);
+    modifier onlyOwnerOrManufacturer {
+        require(((msg.sender == ownerID) || (isManufacturer[msg.sender] == true)), "Only owner or partner manufacturer can call this function");
+        _;
     }
 
-    function removeManufacturer (address m) onlyOwnerOrManufacturer public {
-        bool exists = false;
-        for (uint i = 0; i < manufacturers.length; i++) {
-            if (m == manufacturers[i]) {
-                delete manufacturers[i];
-                exists = true;
-                break;
+
+
+    function _removeFromAddressArray (address[] array, address value) internal {
+        for (uint i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+              array[i] = array[array.length - 1];
+              delete array[array.length - 1];
+              array.length = array.length - 1;
             }
         }
-        if (exists == true) {
-            Manufacturer mInstance = Manufacturer(m);
-            mInstance.removeVendor(id);
+    }
+
+    function _addPendingItem (address iID) internal {
+        require(isPendingItem[iID] == false, "Item is already on the pending list");
+
+        pendingItems.push(iID);
+        isPendingItem[iID] = true;
+    }
+
+    function _addManufacturer (address mID) internal {
+        require(isManufacturer[mID] == false, "Manufacturer is arleady partnered with");
+
+        manufacturers.push(mID);
+        isManufacturer[mID] = true;
+    }
+
+    function receiveManufacturer () public {
+        address mID = msg.sender;
+        require(isPendingManufacturer[mID] == false, "Partnership request has already been sent");
+
+        pendingManufacturers.push(mID);
+        isPendingManufacturer[mID] = true;
+    }
+
+
+
+    function addManufacturer (address mID) onlyOwner public {
+        if (isPendingManufacturer[mID] == true) {
+            _addManufacturer(mID);
+            _removeFromAddressArray(pendingManufacturers, mID);
+            isPendingManufacturer[mID] = false;
+        } else {
+            Manufacturer mInstance = Manufacturer(mID);
+            mInstance.receiveVendor();
         }
+    }
+
+    function removeManufacturer (address mID) onlyOwnerOrManufacturer public {
+        require(isManufacturer[mID] == true, "No such manufacturer");
+
+        _removeFromAddressArray(manufacturers, mID);
+        isManufacturer[mID] = false;
+
+        Manufacturer mInstance = Manufacturer(mID);
+        mInstance.removeVendor(id);
     }
 }
 
@@ -188,10 +284,16 @@ contract ServiceCenter {
     string name;
     string physicalAddress;
     string registrationNumber;
+
     address[] manufacturers;
     address[] pendingManufacturers;
-    address[] requests;
-    mapping(address => address) product;
+
+
+
+    mapping (address => bool) isManufacturer;
+    mapping (address => bool) isPendingManufacturer;
+
+
 
     constructor (string memory _name, string memory _physicalAddress, string memory _registrationNumber) public {
         ownerID = msg.sender;
@@ -200,114 +302,65 @@ contract ServiceCenter {
         registrationNumber = _registrationNumber;
     }
 
-    function addManufacturer (address m) onlyOwner public {
-        for (uint i = 0; i < pendingManufacturers.length; i++) {
-            if (m == pendingManufacturers[i]) {
-                manufacturers.push(m);
-                delete pendingManufacturers[i];
-                return;
+
+
+    modifier onlyOwner {
+        require(msg.sender == ownerID, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyOwnerOrManufacturer {
+        require(((msg.sender == ownerID) || (isManufacturer[msg.sender] == true)), "Only owner or partner manufacturer can call this function");
+        _;
+    }
+
+
+
+    function _removeFromAddressArray (address[] array, address value) internal {
+        for (uint i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+              array[i] = array[array.length - 1];
+              delete array[array.length - 1];
+              array.length = array.length - 1;
             }
         }
-        Manufacturer mInstance = Manufacturer(m);
-        mInstance.receiveServiceCenter(id);
     }
 
-    function receiveManufacturer (address m) public {
-        for (uint i = 0; i < pendingManufacturers.length; i++) {
-            if (m == pendingManufacturers[i]) {
-                return;
-            }
+    function _addManufacturer (address mID) internal {
+        require(isManufacturer[mID] == false, "Manufacturer is arleady partnered with");
+
+        manufacturers.push(mID);
+        isManufacturer[mID] = true;
+    }
+
+    function receiveManufacturer () public {
+        address mID = msg.sender;
+        require(isPendingManufacturer[mID] == false, "Partnership request has already been sent");
+
+        pendingManufacturers.push(mID);
+        isPendingManufacturer[mID] = true;
+    }
+
+
+
+    function addManufacturer (address mID) onlyOwner public {
+        if (isPendingManufacturer[mID] == true) {
+            _addManufacturer(mID);
+            _removeFromAddressArray(pendingManufacturers, mID);
+            isPendingManufacturer[mID] = false;
+        } else {
+            Manufacturer mInstance = Manufacturer(mID);
+            mInstance.receiveServiceCenter();
         }
-        pendingManufacturers.push(m);
     }
 
-    function removeManufacturer (address m) onlyOwnerOrManufacturer public {
-        bool exists = false;
-        for (uint i = 0; i < manufacturers.length; i++) {
-            if (m == manufacturers[i]) {
-                delete manufacturers[i];
-                exists = true;
-                break;
-            }
-        }
-        if (exists == true) {
-            Manufacturer mInstance = Manufacturer(m);
-            mInstance.removeServiceCenter(id);
-        }
-    }
+    function removeManufacturer (address mID) onlyOwnerOrManufacturer public {
+        require(isManufacturer[mID] == true, "No such manufacturer");
 
+        _removeFromAddressArray(manufacturers, mID);
+        isManufacturer[mID] = false;
 
-    function putRequest(address _owner, address _item) {
-        for(uint i = 0; i < reqests.length; i++){
-         if( requests[i] = _owner)
-         return;
-    }
-        reqests.push(_owner);
-        product[requests[requests.length-1]=_item;
-    }
-
-    function checkRequests() returns(bool){
-        for(uint i = 0; i < requests.length; i++){
-            for(uint j = 0; j < manufacturers.length; j++){
-            if(request[i].manufacturerID == manufactures[j])
-            return true;
-            }
-        }
-        return false;
-    }
-
-}
-
-contract User {
-     address id = address(this);
-     address owner;
-     address[] offers;
-     mapping (address => address) offerToOwner;
-
-    constructor (){
-        owner = msg.sender;
-    }
-
-     function buy(address _item, address owner) {
-     Item i = Item(_item);
-     i.newOwner(owner);
-     }
-
-
-     function present(address _item, address _newOwner) {
-     require (msg.sender == owner);
-     Item i = Item(_item);
-     i.newOwner(_newOwner);
-     }
-
-     function sell(address _item, address _newOwner){
-     require (msg.sender == owner);
-     User u = User(_newOwner);
-     for(uint i = 0; i < offers.lenght; i++){
-            if(offers[i] == _newOwner && offerToOwner[offers[i]] == _item){
-            delete offerToOwner[offers[i]];
-            delete offers[i];
-            Item i = Item(_item);
-            i.newOwner(_newOwner);
-            return;
-            }
-        }
-     }
-
-
-     function Offer (address _newOwner, address _item) {
-       User u = User(_newOwner);
-       u.addOffer(_item);
-     }
-
-
-     function addOffer( address _item) {
-        offers.push(_newOwner);
-        offerToOwner[offers[offers.length-1]] = _item;
-      }
-
-    function addRequest(address _SC, address _item) {
-        ServiceCenter SC =  ServiceCenter(_SC);
-        SC.putRequest(owner, _item);
+        Manufacturer mInstance = Manufacturer(mID);
+        mInstance.removeServiceCenter(id);
     }
 }
