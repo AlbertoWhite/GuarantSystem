fffedb78c6e347cc89185024b2cc62d510b64371
Slava-bull <slava-bull@mail.ru>
2019-01-29 09:26:47 +0700

    add rendering

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const signup = require('./signup');
+const signin = require('./signin');
+const guarantee = require('./guarantee');
+
+
+router.get('/', function (req, res) {
+  res.render('index.html');
+});
+
+router.use('/signup', signup);
+router.use('/signin', signin);
+router.use('/guarantee', guarantee);
 
 
 
-  module.exports = router;
+module.exports = router;
