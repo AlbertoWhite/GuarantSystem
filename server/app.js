3d93e0396283fb1af36604d38b55661dc26cb614
Slava-bull <slava-bull@mail.ru>
2019-01-29 09:04:33 +0700

    create server

--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser  = require('body-parser');
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const routes = require('./routes');
 
-app.get('/gg', function (req, res) {
-  res.send('Hello World!GG');
+app.engine('html', function (filePath, options, callback) {
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));
+    return callback(null, content.toString());
+  });
 });
+app.set('view engine', 'html');
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', routes);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
