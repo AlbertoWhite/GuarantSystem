0b9cf49bef664584a7bf7aada9afc831083564cd
zhenyazhd <zhenyazhdarkin@gmail.com>
2019-01-29 17:10:09 +0700

    Contract Update.

--- blockchain/contract.sol
+++ blockchain/contract.sol
@@ -284,14 +284,24 @@ contract ServiceCenter {
     string name;
     string physicalAddress;
     string registrationNumber;
+    
+    struct products {
+        address id; 
+        address items[]; 
+    }
 
     address[] manufacturers;
     address[] pendingManufacturers;
+    address[] requests;
 
 
 
     mapping (address => bool) isManufacturer;
     mapping (address => bool) isPendingManufacturer;
+    mapping (address => bool) isPendingUser;
+    mapping (address => adress)  manufactureOfProduct;
+    mapping(address => struct) product;
+    mapping (address => bool) items;
 
 
 
@@ -363,4 +373,87 @@ contract ServiceCenter {
         Manufacturer mInstance = Manufacturer(mID);
         mInstance.removeServiceCenter(id);
     }
+   
+    function fromUser(address _owner, address _item, address _manuf) {
+        require (isPendingUser[_owner] != true || items[_item] != true , "Your request is being processed");
+        require (isManufacturers[_manuf] == true, 'We do not cooperate');
+        if (isPendingUser[_owner] == true){
+        product[_owner].item.push(_item);
+        }
+        else{
+        products p;
+        p.id = _owner;
+        p.item.push(_item);
+        product[_owner] = p;
+        }
+    
+     function toUser (address _owner; address _item){
+     User u = User(_owner);
+     u.fromService(address _item);
+      }     
+
 }
+
+contract User {
+     address id = address(this);
+     address owner;
+     address[] offers;
+     address[] SC;
+
+     mapping (address => address) offerToOwner;
+     mapping (address => bool) offersForProduct;
+     mapping (address => bool) requestsFromSC;
+      
+    constructor (){
+        owner = msg.sender;
+    }
+      
+     function sell(address _item, address _newOwner){
+     require (msg.sender == owner, 'You are not owner');
+     User u = User(_newOwner);
+     require (offersForProduct[_newOwner] == true, 'There are no offers form this user');
+     require (offerToOwner[offers[i]] == _item, 'There are no such products form this user');
+      if (offers.length > 1) {
+                    offers[i] = offers[offers.length-1];
+                    offersForProduct[offers[i]]  = true;
+                    offerToOwner[offers[i]] = offerToOwner[offers[offers.length-1]];
+                    delete offerToOwner[offers[offers.length-1]];
+                    delete offers[offers.length-1];
+                }
+                delete offerToOwner[offers[i]];
+                delete offers[i];
+                Item i = Item(_item);
+                i.newOwner(_newOwner);
+                return;
+         
+     }
+
+
+     function Offer (address _newOwner, address _item) { 
+       User u = User(_newOwner);
+       u.addOffer(_item);
+     }
+
+     
+     function addOffer( address _item) {
+        offers.push(_newOwner);
+        offersForProduct[_newOwner]  = true;
+        offerToOwner[offers[offers.length-1]] = _item;
+      }
+    
+    function toService(address _SC, address _item) {
+        Item I = Item(_item);
+        ServiceCenter SC =  ServiceCenter(_SC);
+        SC.fromUser(owner, _item, I.manufacturerID);
+    } 
+
+    function fromService (address _item){
+        require (requestsFromSC[_item] != true, "You already have this require");
+        SC.push(_item);
+        requestsFromSC[_item];      
+}
+    
+}
+
+
+
