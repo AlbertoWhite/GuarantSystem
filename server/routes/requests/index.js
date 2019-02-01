dae5fec3ef140be47daa197548e1cee06da5170a
Slava-bull <slava-bull@mail.ru>
2019-02-01 21:27:21 +0700

    fix bag

--- server/routes/requests/index.js
+++ server/routes/requests/index.js
@@ -8,7 +8,7 @@ router.get('/list', function (req, res) {
   res.render('requests/list.html',{listOfRequests : [{_id:'1',info: 'requests1'},{_id:'2',info: 'requests2'}]});//TODO tmp
 });
 
-router.post('/apply/:reqId',function(req,res){
+router.get('/apply/:reqId',function(req,res){
   res.redirect('/requests/list');
 });
 
