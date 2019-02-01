415a515d91dcd4e2b7bb60655d23f5f21a2d0654
Slava-bull <slava-bull@mail.ru>
2019-02-01 21:22:05 +0700

    bag fix,add id to requests

--- server/routes/requests/index.js
+++ server/routes/requests/index.js
@@ -5,10 +5,10 @@ var mypass = require('../../MyPassport');
 router.use(mypass.mustAuthenticatedMw);
 
 router.get('/list', function (req, res) {
-  res.render('requests/list.html',{listOfRequests : ["requests1","requests2"]});//TODO tmp
+  res.render('requests/list.html',{listOfRequests : [{_id:'1',info: 'requests1'},{_id:'2',info: 'requests2'}]});//TODO tmp
 });
 
-router.post('apply/:reqId',function(req,res){
+router.post('/apply/:reqId',function(req,res){
   res.redirect('/requests/list');
 });
 
