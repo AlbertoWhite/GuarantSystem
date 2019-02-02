0c3a4cc38afbbc0f0ac006d2cee0564662146f0e
Viktor Gronskiy <v.gronskiy@cogia.de>
2019-02-02 18:55:22 +0700

    ItemCtrl and ManufacturerCtrl added

---
+++ web3/Controllers.js
@@ -0,0 +1,14 @@
+const MainCtrl = require('./MainCtrl');
+const UserCtrl = require('./UserCtrl');
+const VendorCtrl = require('./VendorCtrl');
+const ManufacturerCtrl = require('./ManufacturerCtrl');
+const ServiceCenterCtrl = require('./ServiceCenterCtrl');
+
+
+module.exports = {
+  Main: MainCtrl,
+  User: UserCtrl,
+  Vendor: VendorCtrl,
+  Manufacturer: ManufacturerCtrl,
+  ServiceCenter: ServiceCenterCtrl,
+}
---
+++ web3/UserCtrl.js
@@ -0,0 +1,10 @@
+module.exports = {
+  userList: function () {},
+  manufacturerList: function () {},
+  vendorList: function () {},
+  serviceCenterList: function () {},
+  registerUser: function () {},
+  registerManufacturer: function () {},
+  registerVendor: function () {},
+  registerServiceCenter: function () {}
+}
