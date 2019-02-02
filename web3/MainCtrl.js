d79025373308125694db924d544a3f2cf72f1c65
Viktor Gronskiy <v.gronskiy@cogia.de>
2019-02-02 19:10:50 +0700

    MainCtrl placeholder

---
+++ web3/MainCtrl.js
@@ -0,0 +1,10 @@
+module.exports = {
+  userList: function () {},
+  manufacturerList: function () {},
+  vendorList: function () {},
+  serviceCenterList: function (publicKey /*args*/) {/* returns txAddress */},
+  registerUser: function (publicKey) {/* returns txAddress */},
+  registerManufacturer: function (publicKey /*args*/) {/* returns txAddress */},
+  registerVendor: function (publicKey /*args*/) {/* returns txAddress */},
+  registerServiceCenter: function (publicKey /*args*/) {/* returns txAddress */}
+}
