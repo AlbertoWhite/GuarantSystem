8c6260e215690e52e42d279744d4a26a8e1cfe52
mano7onam <andrey.matveev.mano@gmail.com>
2019-02-03 12:37:54 +0700

    Add python web3 file

---
+++ web3/python/web3python.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys, getopt
+
+def registerManufacturer(ownerID, name, physicalAddress, registrationNumber):
+    print("impl me please")
+    pass
+
+argv = sys.argv
+if (argv[1] == 'registerManufacturer') {
+    registerManufacturer(argv[2], argv[3], argv[4], argv[5])
+}
+
+print(sys.argv)
+
+
+
